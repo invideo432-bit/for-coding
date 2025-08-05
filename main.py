6fd2bb7c9351cf54cfa4b7e46f2733bee2c49780
invideo432-bit <invideo432@gmail.com>
2025-08-05 11:37:17 +0500

    dxfdfsd

---
+++ main.py
@@ -0,0 +1,381 @@
+# print("Hi hello")
+# print("what is your name: ")
+# print("code college ")
+#
+# print("ALLAH")
+# print("What is my name?")
+# print("Allah"+ "Muhammad")
+# age = 87
+#
+# print(age)
+# print("My name is Saim Abdullah")
+# print("What is Your name")
+# age2= 86
+# l
+from operator import is_not
+
+# name = "Arslan"
+# print(name[5])
+
+
+# name = "Arslan"
+# age = 22
+#salary = 234.2
+#print(type(salary))
+#is_disabled = True
+#print(type(is_disabled))
+#age =  65
+#print(type(age))
+#pay = 234.048
+#print(type(pay))
+# #print(type(is_disabled))
+
+
+# num = input("enter a four digit number:")
+# first_digit = int(num[0])
+# second_digit = int(num[1])
+# third_digit = int(num[2])
+# fourth_digit = int(num[3])
+# result = first_digit - second_digit + third_digit + fourth_digit
+# print(result)
+# a = 10
+# b = 20
+
+# Do Not change the above section
+
+#
+# a,b=b,a
+
+
+
+# Also dont change this
+# print(f"The value of a is {a}")
+# print(f"The value of b is {b}")
+
+
+# a = "123"
+# a = int(a)
+# print(type(a))
+#
+# height = input("Please enter your height")
+# height = int(height)
+# bill = 0
+# if height >= 120:
+#     print("you can take ride")
+#     age = int(input("Please enter your age"))
+#     if age >= 18:
+#         print("you have to pay 200")
+#         bill = 200
+#
+#     elif age >= 12:
+#         print("you have to pay 100")
+#         bill = 100
+#     else:
+#         print("you have to pay 50")
+#         bill = 50
+#     want_photo = input("Do you want photo? 'y' or 'n'").lower()
+#     if want_photo == 'y':
+#         bill = bill + 50
+#     print(f"Your bill is {bill}")
+# else:
+#     print("you can't take ride")
+# height = int (input("please enter you height"))
+# if height >= 124:
+#     print ("you can  swim")
+#     bank_balance = int(input("How much money do you have?"))
+#     if bank_balance >= 1500:
+#         print("you can buy a cottage at island")
+#         bill = 1500
+# elif height <= 123:
+#     print("you can only go inside the water but cannot swim")
+#     print("you have to pay $1000")
+#     bill = 1000
+# else:
+#     print("you can't swim")
+# want_photo = input("Do you want a photo?'YES' or 'NO'")
+# if want_photo == 'YES':
+#     print("you have to pay $10")
+#     bill = 10
+# print(bill)
+# #
+# #
+#
+# age = input("Enter your age: ")
+# age = int(age)
+# if age >= 18:
+#     print("you have to pay 200")
+# elif age <= 12
+#     print("you have to pay 100")
+# else:
+#     print("you cannot ride")
+# height = input("Enter your height: ")
+# height = int(height)
+# if height >= 100:
+#     print("you have to pay 1000")
+# else :
+#     print("you cannot ride")
+
+
+
+# num = int(input("Enter a number: "))
+# if num % 2 == 0:
+#     print("The number is even")
+# elif num % 2 != 1:
+#     print("The number is odd")
+
+
+
+
+# a = 12
+# b = 12.12
+# c = True
+#
+# print(f"This is the value of a is  {a}")
+#
+#
+#
+# num = int(input("Enter a number: "))
+# if num % 2 == 0:
+#     print("Even")
+# else:
+#     print("Odd")
+# #
+# year= int(input("Enter year: "))
+#
+# if year % 4 == 0:
+#     if year % 100!= 0:
+#         if year % 400 == 0:
+#             print("leap year")
+#         else:
+#             print("not leap year")
+#     else:
+#         print("Leap Year")
+#
+# else:
+#     print("Not a leap year")
+
+
+
+
+
+# marks = int(input("Enter marks: "))
+# if marks >= 80:
+#     print("you are eligible for admission")
+#     cs_marks = int(input("Enter cs marks: "))
+#     math_marks = int(input("Enter math marks: "))
+#     physics_marks = int(input("Enter physics marks: "))
+#     if cs_marks >= 80:
+#         print("you are eligible for Cs")
+#     if math_marks >= 80:
+#         print("you are eligible for math")
+#     if physics_marks >= 80:
+#         print("you are eligible for physics")
+# else:
+#     print("you are not eligible for admission")
+# bill = 0
+# size = input("What size of do you want?'S' ,'L', 'M'").upper()
+# if size == 'S':
+#     print("you have to pay $15")
+#     bill += 15
+#     print(bill)
+# elif size=='M':
+#     print("you have to pay $20 ")
+#     bill +=20
+#     print(bill)
+# else:
+#     print("you have to pay $25")
+#     bill += 25
+#     print(bill)
+# want_pepperoni = input("you want pepperoni? ")
+# if   want_pepperoni =='y':
+#     print("do you want pepperoni?'y','n'").lower()
+# if  size == 'S':
+#         bill+= 2
+#
+#       elif size == 'M':
+#         bill+= 3
+#         print(bill)
+#       else:
+#           bill+= 3
+#           print(bill)
+#     want_cheese = input("Do you want cheese?'y' or 'n'")
+#     if want_cheese =='y':
+#             bill+= 1
+#             print(bill)
+# print(f"you have to pay ${bill}")
+# bill =0
+# Pizza = input("which pizza do you want?'s','m','l':").lower()
+# if Pizza == "s":
+#     print("you have to pay $15")
+#     bill += 15
+# elif Pizza == "m":
+#     print("you have to pay $20")
+#     bill += 20
+# else:
+#     print("you have to pay $25")
+#     bill += 25
+#     print("bill")
+# want_pepperoni = input("do you want pepperoni?:").lower()
+# if want_pepperoni == "y":
+#     if Pizza == "s":
+#         bill += 2
+#     else:
+#         bill += 3
+# extra_cheez = input("do you want extra cheez?:").lower()
+# if extra_cheez == "y":
+#     bill += 1
+
+# print("way to find ALLAH")
+# # print(f"Total bill is : {bill}")
+# print("Way to find Allah")
+# choice1=input("where you want to go? 'right 'or 'left'")
+# if choice1 == 'right ':
+#     print("you are going ont the right way")
+#     choice2 = input("")
+# else:
+#     print("you are going ont the wrong way)
+
+#
+
+# randint
+
+#
+# print("Welcome to the castle of  Islam")
+# print("You are going to find the path of satan or the of Allah")
+# print("you have two Choice🤔")
+# print("1.Want to go to the path of satan")
+# print("2.Want to go to path of Allah")
+# choice =int(input("What is your choice?'1','2'"))
+# if choice ==1:
+#     print("You are on the path of ALLAH")
+#     print("You are the right person with right aim")
+#     print("Now you have two more choices")
+#     print("1.Follow the order of Allah")
+#     print("2.Follow the order of Muhammad(P.B.U.H)")
+#     Allah_order = int(input("What is your choice?'1','2'"))
+#     if Allah_order   == 1:
+#         print("You are following the order of Allah")
+#     else:
+#         print("You are the disobeyer of Allah")
+#         print("You will be on the way of hell")
+# else:
+#     print("You are on the path of Satan")
+# import random
+# random_integer = random.randint(1, 100)
+# print(random_integer)
+# import random
+# random_number = random.randint(1, 2)
+# if random_number == 1:
+#     print("head")
+# else:
+#     print("tail")
+# user_input = int(input("choose one of them:'head','tail'1 for head and 2 for tail"))
+# if user_input == random_number:
+#     print("Both are same")
+# else:
+#     print("Both are different")
+import random
+from os import device_encoding
+
+#
+
+#
+# student_marks = [33,66,44,33,66,77,88,96,37]
+# total_marks = 0
+# for  mark in student_marks:
+#     total_mark = total_marks + mark
+#
+# print(total_marks)
+#
+####
+# count = 0
+# for mark in student_marks:
+#     count = count + 1
+# average = total_marks / count
+# print(round(average))
+
+
+# student_name = ["ali", "usama","maki"]
+# for name in student_name:
+#     print(name)
+# student_marks = [54,45,45,78,54,987,236,90, -45]
+# heighest_value= student_marks[0]
+# for mark in student_marks:
+#     if mark > heighest_value:
+#         heighest_value = mark
+#
+# print(heighest_value)
+# lowest_marks = student_marks[0]
+# for mark in student_marks:
+#     if mark < lowest_marks:
+#         lowest_marks = mark
+#
+# print(lowest_marks)
+# user_choice = input("What size of pizza do you want?'s','m','l'").lower()
+# bill = 0
+# if user_choice == 's':
+#     bill = bill + 800
+# elif user_choice == 'm':
+#     bill = bill + 1200
+# else:
+#     bill = bill + 1500
+# want_pepperoni = input("Do you want pepperoni? 'y' or 'n'")
+# if want_pepperoni == 'y':
+#     if user_choice == 's':
+#       bill = bill + 50
+#     elif user_choice == 'm':
+#         bill = bill + 100
+#     else:
+#         bill = bill + 200
+# extra_cheese = input("Do you want extra cheese? 'y' or 'n'")
+# if extra_cheese == 'y':
+#     if user_choice == 's':
+#         bill = bill + 80
+#     else:
+#         bill = bill + 100
+# print(f"Your final bill is: {bill}")
+# import random
+# student_names = ["ALI","AHMAD","SAAD","TAHIR","SAIM","UMAIR","RAUF"]
+# print(student_names)
+# a = random.randint(0,6)
+# print(a)
+# for a in range (1,20):
+#     print(a)
+# for a in range (1,20,4):
+#     print(a)
+# for b in range (1,20):
+#      print("Saim")
+# for c in range (1,20,3):
+#     print("Saim")
+# for d in range (1,20):
+#     print("d")
+# total = 0
+for a in range (2,20,2):
+    print(a)
+total = 0
+for a in range(1,201):
+    if a % 2 == 0:
+        print(a)
+        total += a
+        print("sum of even numbers from 2 to 200 is: ", total)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
